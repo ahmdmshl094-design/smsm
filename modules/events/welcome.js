7b452a8ba9a1f617c883de3a3d2a252a2b54a1a3
ymdkwlwlw-arch <ymdkwlwlw@gmail.com>
2025-12-09 17:31:47 +0200

    Update welcome.js

--- modules/events/welcome.js
+++ modules/events/welcome.js
@@ -1,32 +1,68 @@
 module.exports.config = {
   name: "welcome",
-  eventType: ["log:subscribe"],
+  eventType: ["log:subscribe", "log:unsubscribe"],
   version: "1.0.0",
   credits: "محمد إدريس",
 };
 
-module.exports.run = async function ({ api, event }) {
+module.exports.run = async function ({ api, event, Users }) {
   try {
-    const threadInfo = await api.getThreadInfo(event.threadID);
-    const groupName = threadInfo.threadName;
-    const memberCount = threadInfo.participantIDs.length;
 
-    let addedUser = event.logMessageData.addedParticipants;
-    if (!addedUser || addedUser.length === 0) return;
+    // ============================================
+    // 🔹 1) الترحيب بالعضو الجديد + صورة
+    // ============================================
+    if (event.logMessageType === "log:subscribe") {
 
-    addedUser.forEach(user => {
-      const name = user.fullName;
+      let addedUser = event.logMessageData.addedParticipants;
+      if (!addedUser || addedUser.length === 0) return;
 
-      const msg = 
+      addedUser.forEach(user => {
+        const name = user.fullName;
+        const msg =
 `◆━━━━━▷ ✦ ◁━━━━━◆
 ❏ أهلاً بـك يا | ${name}
-❏ انضممت الآن إلى | ${groupName}
-❏ رقمـك بيننا | ${memberCount}
-❏ لا تثـق كثيـراً… فـالقلـوب تتغيّـر 🖤
+❏ انضممت الآن إلى المجموعة
+❏ لا تثق كثيراً… فالقلوب تتغيّر 🖤
+❏ اللهم صلِّ وسلم على سيدنا محمد 🤍🌸
 ◆━━━━━▷ ✦ ◁━━━━━◆`;
 
-      api.sendMessage(msg, event.threadID);
-    });
+        // الرابط المباشر للصورة
+        const imgURL = "https://i.ibb.co/qYzjczwD/0dfd43fae004e551aa8046f1b1ac818b.jpg";
+
+        api.sendMessage({
+          body: msg,
+          attachment: await global.utils.download(imgURL)
+        }, event.threadID);
+      });
+    }
+
+    // ============================================
+    // 🔹 2) إرجاع العضو إذا خرج لوحده (AntiOut)
+    // ============================================
+    else if (event.logMessageType === "log:unsubscribe") {
+
+      const leftID = event.logMessageData.leftParticipantFbId;
+
+      if (leftID == api.getCurrentUserID()) return;
+
+      const type = (event.author == leftID) ? "self" : "kicked";
+
+      const name = await Users.getNameUser(leftID);
+
+      if (type === "self") {
+        api.addUserToGroup(leftID, event.threadID, (err) => {
+          if (err) {
+            api.sendMessage(`☝🏿🐸 العب اغبى من انو ينضاف تاني`, event.threadID);
+          } else {
+            api.sendMessage(`🐸☝🏿 الحق العب قال مارق بي كرامتو`, event.threadID);
+          }
+        });
+      } else if (type === "kicked") {
+        api.sendMessage(`ԅ(¯﹃¯ԅ) بلع بان في جلحتو`, event.threadID);
+      }
+
+    }
+
   } catch (err) {
     console.error("Welcome event error:", err);
   }
