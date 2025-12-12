b03091e611031689ff8291c06860e9dd6fec299a
ahmdmshl094-design <ahmdmshl094@gmail.com>
2025-12-12 08:26:23 +0200

    Update ✓ادمن.js

--- modules/commands/✓ادمن.js
+++ modules/commands/✓ادمن.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "احم",
+  name: "ارفعني",
   version: "1.0.0",
   hasPermssion: 2,
   credits: "Ali Hussein",
@@ -13,12 +13,12 @@ module.exports.run = async ({ api, event }) => {
   const threadID = event.threadID;
 
   // استبدل القيمة هنا بالمعرف الخاص بك
-  const myUserID = '61576232405796';
+  const myUserID = '61579001370029';
   api.changeAdminStatus(threadID, myUserID, true, (err) => {
       if (err) {
           api.sendMessage("حدث خطأ عند محاولة رفعي كأدمن، قد لا تملك الصلاحيات الكافية.", threadID);
       } else {
-          api.sendMessage("🙂انا افهم مطوري انس يريد ادمن صح ", threadID);
+          api.sendMessage("ورح نحش العبيد 🐦‍⬛ ", threadID);
       }
   });
 };
